723159d0aa968413c151f0862ae438d78d85399c
JafarAssegaf <ajib.aiwa@gmail.com>
2024-05-28 15:26:53 +0700

    feat:intro ~ Add Intro app feature

--- app/src/main/java/com/bangkit2024/facetrack/ui/intro/IntroActivity.kt
+++ app/src/main/java/com/bangkit2024/facetrack/ui/intro/IntroActivity.kt
@@ -3,13 +3,23 @@ package com.bangkit2024.facetrack.ui.intro
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.bangkit2024.facetrack.R
+import com.bangkit2024.facetrack.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var introBinding: ActivityIntroBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+
+        introBinding = ActivityIntroBinding.inflate(layoutInflater)
+        setContentView(introBinding.root)
+
+        introBinding.btnNextIntro3.setOnClickListener {
+            // Intent to LoginActivity
+            //
+            // startActivity(Intent(this, LoginActivity::class.java))
+            // finish()
+        }
     }
 }
--- app/src/main/java/com/bangkit2024/facetrack/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/bangkit2024/facetrack/ui/splash/SplashActivity.kt
@@ -9,15 +9,20 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.bangkit2024.facetrack.R
+import com.bangkit2024.facetrack.databinding.ActivitySplashBinding
 import com.bangkit2024.facetrack.ui.intro.IntroActivity
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var splashBinding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        splashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
 
         // This is temporary solution before implement API and Store token
         lifecycleScope.launch {
