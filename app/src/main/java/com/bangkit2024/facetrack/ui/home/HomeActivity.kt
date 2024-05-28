2ff84ba900c17ee2bf6b97fa3367baa79e5bf7b0
JafarAssegaf <ajib.aiwa@gmail.com>
2024-05-28 09:16:07 +0700

    refactor:structure ~ Add structure project a

---
+++ app/src/main/java/com/bangkit2024/facetrack/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit2024.facetrack.ui.home
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bangkit2024.facetrack.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
