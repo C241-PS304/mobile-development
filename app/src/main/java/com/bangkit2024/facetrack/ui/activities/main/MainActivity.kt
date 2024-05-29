cf7ff1b76061d455de8d8e34ba4a9abc031ca6f3
Gerald <dzulfiqarg@gmail.com>
2024-05-29 23:06:52 +0700

    feat:Authentication ~add Login and Register

--- app/src/main/java/com/bangkit2024/facetrack/ui/activities/main/MainActivity.kt
+++ app/src/main/java/com/bangkit2024/facetrack/ui/activities/main/MainActivity.kt
@@ -2,7 +2,6 @@ package com.bangkit2024.facetrack.ui.activities.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.bangkit2024.facetrack.R
