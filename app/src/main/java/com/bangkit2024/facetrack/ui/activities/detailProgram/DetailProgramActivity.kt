3c0312155b4932b7deb6547a9e76c55811d74c6d
JafarAssegaf <ajib.aiwa@gmail.com>
2024-06-05 10:10:58 +0700

    feat:detail_program ~ Slicing UI Detail Program

--- app/src/main/java/com/bangkit2024/facetrack/ui/activities/detailProgram/DetailProgramActivity.kt
+++ app/src/main/java/com/bangkit2024/facetrack/ui/activities/detailProgram/DetailProgramActivity.kt
@@ -1,11 +1,7 @@
 package com.bangkit2024.facetrack.ui.activities.detailProgram
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.bangkit2024.facetrack.R
 import com.bangkit2024.facetrack.databinding.ActivityDetailProgramBinding
 
 class DetailProgramActivity : AppCompatActivity() {
